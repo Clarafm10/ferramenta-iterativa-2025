50fcad5d8e341e8d76e8d35dad61589320b1e552
Clarafm10 <clara.moreschi@escola.pr.gov.br>
2025-09-17 16:16:40 -0300

    17/09

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 function criaCartao(categoria, pergunta, respota) {
     console.log(categoria, pergunta, respota)
+
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    //17-09-25//
+    cartao.innerHTML = `
+    <div class="cartao conteudo">
+        <h3>$(categoria)</h3>
+        <div class="cartao_contudo_pergunta">
+            <p>$(pergunta)</p>
+        </div>
+        <div class="cartao_contudo_pergunta">
+            <p>$(resposta)</p>
+    </div>
+
+</div>
+`
 }
