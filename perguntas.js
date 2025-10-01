22837b42ab5559905c4e762e862502fee76978c2
Clarafm10 <clara.moreschi@escola.pr.gov.br>
2025-10-01 15:56:33 -0300

    01/10

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,22 @@
-criacartao{
+criaCartao{
     'Funções de linguagem',
     'O que é a função metalinguítica?',
     'ocorre quando a linguagem é usada para falar sobre a própria linguagem ou o próprio código comunicativo'
 }
 
-criacartao{
+criaCartao{
     'Funções de linguagem',
     'O que é a função fática?',
     'privilegia a interação entre emissor e receptor das mensagens, ou seja, entre o locutor e o interlocutor. '
 }
 
-criacartao{
+criaCartao{
     'Funções de linguagem',
     'O que é a função refrencial?',
     'tem como objetivo transmitir informações de forma objetiva e direta, focando no contexto da comunicação . '
 }
 
-criacartao{
+criaCartao{
     'Funções de linguagem',
     'O que é a função conativa?',
     ' foca no receptor da mensagem, buscando persuadi-lo a realizar uma ação, a aceitar uma ideia ou a mudar seu comportamento . '
