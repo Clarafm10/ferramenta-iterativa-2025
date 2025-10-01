5a1f88392cabcfd1c947fd1eba9119edad0edea8
Clarafm10 <clara.moreschi@escola.pr.gov.br>
2025-10-01 15:23:20 -0300

    01/10

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+criacartao{
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+}
+
+criacartao{
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+}
